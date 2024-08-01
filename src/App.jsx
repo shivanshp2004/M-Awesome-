7a3fadb1dac2258a8bd6c1fb28704a391b770f09
shivanshp2004 <shivanshpathak9988@gmail.com>
2024-08-01 20:25:59 +0530

    responsiveness ensured

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Input from "./Components/Input"
 import Card from './Components/Card'
 function App() {
   return (
-    <div className='w-full h-screen bg-[#e9c46a]'>
+    <div className='w-full h-screen bg-[#e9c46a] max-sm:h-screen max-sm:overflow-hidden'>
     <div className='flex w-full h-[80px] bg-[#344e41] justify-center items-center text-white text-[1.8rem]'>
       <h1 className='text-white font-bold text-[2.2rem]'>M(Awesome).com</h1>
     </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,59 +1,71 @@
-import React, { useContext, useState } from 'react'
-import { Context } from './Context'
+import React, { useContext, useState } from "react";
+import { Context } from "./Context";
 const Card = () => {
-    const context=useContext(Context)
-    const URL="https://api.weatherapi.com/v1/current.json?key=571bfa32950a47e7be0165325243007&q=";
-    const [temp,setTemp]=useState(0)
-    const [feelsLike,setFeelsLike]=useState(0)
-    const [region,setRegion]=useState("MP")
-    const [cloud,setCloud]=useState(0)
-    const [tempType,setTempType]=useState("")
-    const [wind,setWind]=useState(0)
-    const[temp_f,setTemp_f]=useState(0)
-    const[feelsLike_f,setFeelsLike_f]=useState(0)
-    const [city,setCity]=useState(context.finalCity)
-    const[image,setImage]=useState()
-    const [humidity,setHumidity]=useState(0)
-    const findTemp=async ()=> {
-      const response=await fetch(`${URL}${context.finalCity}&aqi=yes`)
-      const json=await response.json()
-      setTemp(json.current.temp_c)
-      setTemp_f(json.current.temp_f)
-      setFeelsLike(json.current.feelslike_c)
-      setFeelsLike_f(json.current.feelslike_f)
-      setRegion(json.location.region)
-      setTempType(json.current.condition.text)
-      setCity(json.location.name)
-      setHumidity(json.current.humidity)
-      setImage(json.current.condition.icon)
-      setCloud(json.current.cloud)
-      setWind(json.current.wind_kph)
-      if(cloud<10) setCloud(cloud=>cloud-10)
-      console.log(json)
-    }
-    findTemp()
+  const context = useContext(Context);
+  const URL =
+    "https://api.weatherapi.com/v1/current.json?key=571bfa32950a47e7be0165325243007&q=";
+  const [temp, setTemp] = useState(0);
+  const [feelsLike, setFeelsLike] = useState(0);
+  const [region, setRegion] = useState("MP");
+  const [cloud, setCloud] = useState(0);
+  const [tempType, setTempType] = useState("");
+  const [wind, setWind] = useState(0);
+  const [temp_f, setTemp_f] = useState(0);
+  const [feelsLike_f, setFeelsLike_f] = useState(0);
+  const [city, setCity] = useState(context.finalCity);
+  const [image, setImage] = useState();
+  const [humidity, setHumidity] = useState(0);
+  const findTemp = async () => {
+    const response = await fetch(`${URL}${context.finalCity}&aqi=yes`);
+    const json = await response.json();
+    setTemp(json.current.temp_c);
+    setTemp_f(json.current.temp_f);
+    setFeelsLike(json.current.feelslike_c);
+    setFeelsLike_f(json.current.feelslike_f);
+    setRegion(json.location.region);
+    setTempType(json.current.condition.text);
+    setCity(json.location.name);
+    setHumidity(json.current.humidity);
+    setImage(json.current.condition.icon);
+    setCloud(json.current.cloud);
+    setWind(json.current.wind_kph);
+    if (cloud < 10) setCloud((cloud) => cloud - 10);
+  };
+  findTemp();
   return (
-    <div className='w-full h-[70%] flex justify-center items-center mt-[20px] flex-col'>
-        <p className='text-[2.5rem] font-bold ml-[10px] mb-[20px] '>{city}, {region}</p>
-     <div className='w-[450px] h-[550px] bg-[#ef233c] shadow-[0px_0px_5px_8px_rgba(0,0,0,0.15)] rounded-3xl'>
-      <div className='flex w-[450px] h-[80px] items-center justify-between'>
-        <div className='w-[100px] mt-[50px] bg-purple-100 h-[100px] ml-[20px] flex items-center justify-center pd-2 rounded-full p-2'>
-        <img src={image} className='w-[100px] mb-[-3px]'/>
+    <div className="w-full h-[70%] flex justify-center items-center mt-[20px] flex-col max-sm:mt-[60px] max-sm:h-[65%]">
+      <p className="text-[2.5rem] font-bold ml-[10px] mb-[20px] max-sm:ml-[40px]">
+        {city}, {region}
+      </p>
+      <div className="w-[450px] h-[550px] bg-[#ef233c] shadow-[0px_0px_5px_8px_rgba(0,0,0,0.15)] max-sm:w-[370px] max-sm:h-[395px] rounded-3xl">
+        <div className="flex w-[450px] h-[80px] items-center justify-between max-sm:w-[398px] ">
+          <div className="w-[110px] mt-[50px] bg-purple-100 h-[100px] ml-[20px] flex items-center justify-center pd-2 rounded-full p-2 max-sm:w-[110px]">
+            <img src={image} className="w-[100px] mb-[-3px] max-sm:w-[100px]" />
+          </div>
+          <p className="text-[2.0rem] font-bold mt-[40px] mr-[55px] max-sm:mr-[0px] max-sm:ml-[30px] ml-[30px]">
+            {tempType}
+          </p>
         </div>
-        <p className='text-[2.0rem] font-bold ml-[10px] mt-[40px] mr-[55px]'>{tempType}</p>
+        <div className="ml-[10px]">
+          <p className="text-[1.6rem] font-medium ml-[10px] mt-[75px] ">
+            Temperature : {temp}°C / {temp_f}°F
+          </p>
+          <p className="text-[1.6rem] font-medium ml-[10px] mt-[10px] ">
+            Feels like : {feelsLike}°C / {feelsLike_f}°F
+          </p>
+          <p className="text-[1.6rem] font-medium ml-[10px] mt-[10px] ">
+            Humidity : {humidity}
+          </p>
+          <p className="text-[1.6rem] font-medium ml-[10px] mt-[10px] ">
+            Wind Speed : {wind}kph
+          </p>
+          <p className="text-[1.6rem] font-medium ml-[10px] mt-[10px] ">
+            Rain Probability : {cloud - 10}%
+          </p>
+        </div>
       </div>
-      <div className='ml-[10px]'>
-
-        <p className='text-[1.6rem] font-medium ml-[10px] mt-[75px] '>Temperature : {temp}°C / {temp_f}°F</p>
-        <p className='text-[1.6rem] font-medium ml-[10px] mt-[10px] '>Feels like : {feelsLike}°C / {feelsLike_f}°F</p>
-        <p className='text-[1.6rem] font-medium ml-[10px] mt-[10px] '>Humidity : {humidity}</p>
-        <p className='text-[1.6rem] font-medium ml-[10px] mt-[10px] '>Wind Speed : {wind}kph</p>
-        <p className='text-[1.6rem] font-medium ml-[10px] mt-[10px] '>Rain Probability : {cloud-10}%</p>
-      </div>
-
-     </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
